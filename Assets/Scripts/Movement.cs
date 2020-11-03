dec96218fdd26604856531442a9a1bec0dd186b8
Xiubo Zhao <906672365@qq.com>
2020-11-04 00:26:44 +0800

    Agent movement & basic steering

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float maxSpeed = 10;
+    public float maxForce = 100;
+    public Vector3 velocity;
+    public float interval = 0.2f;
+    public Vector3 steeringForce;
+    public Transform destination;
+    private Vector3 acceleration;
+    private float timer;
+
+    private List<SteeringBase> steerings;
+
+    private void Start()
+    {
+        steeringForce = Vector3.zero;
+        timer = 0f;
+        SteeringBase[] steeringComp = GetComponents<SteeringBase>();
+        foreach(SteeringBase sb in steeringComp)
+        {
+            steerings.Add(sb);
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= interval)
+        {
+            CalculateSteering();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        CalculateVelocity();
+        
+    }
+
+    private void CalculateSteering()
+    {
+        foreach(SteeringBase sb in steerings)
+        {
+            steeringForce += sb.Force();
+        }
+    }
+
+    private void CalculateVelocity()
+    {
+        velocity += acceleration * Time.fixedDeltaTime;
+        if (velocity.magnitude > maxSpeed)
+            velocity = velocity.normalized * maxSpeed;
+
+        transform.position += velocity * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/Steerings/SteeringSeek.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringSeek : SteeringBase
+{
+    public GameObject target;
+    private Vector3 desiredVelocity;
+    private float maxSpeed;
+    private Movement agentMovement;
+
+    private void Start()
+    {
+        agentMovement = GetComponent<Movement>();
+        maxSpeed = agentMovement.maxSpeed;
+    }
+
+    public override Vector3 Force()
+    {
+        desiredVelocity = (target.transform.position - transform.position).normalized * maxSpeed;
+        desiredVelocity.y = 0;
+        return desiredVelocity - agentMovement.velocity;
+    }
+}
