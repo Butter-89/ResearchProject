cecef88d7a4588cc9556ca37526ee19193d68e28
Xiubo <906672365@qq.com>
2020-11-13 16:17:04 +0800

    Adding formation v1

---
+++ Assets/Scripts/CustomizedFormation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizedFormation : MonoBehaviour
+{
+    // Pre-defined formation in prefab
+    public GameObject leader;
+    public List<GameObject> members = new List<GameObject>();
+    public float mobilityScale = 1;
+
+    public List<Vector3> relativePositions = new List<Vector3>();
+
+    private void Start()
+    {
+        leader = this.gameObject;
+        Movement[] agentMovementComps = GetComponentsInChildren<Movement>();
+        foreach(Movement agentMovement in agentMovementComps)
+        {
+            GameObject agent = agentMovement.transform.gameObject;
+            members.Add(agent);
+            Vector3 relativePosition = agent.transform.position - transform.position;
+
+            if(agent.GetComponent<SteeringArrive>() == null)
+                agent.AddComponent<SteeringArrive>();
+        }
+    }
+
+    
+    
+}
