bc8300670b779e98e45f97d2d275fe033c9e219c
Xiubo Zhao <906672365@qq.com>
2020-11-14 20:07:47 +0800

    Implemented queue

---
+++ Assets/Scripts/Steerings/SteeringQueue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringQueue : SteeringBase
+{
+    public float max_queueAhead;
+    public float max_queueRadius;
+    public LayerMask layersChecked;
+
+    private Collider[] colliders;
+    private Movement agentMovement;
+    private int layerID;
+    private LayerMask layerMask;
+
+    void Start()
+    {
+        agentMovement = GetComponent<Movement>();
+        layerID = LayerMask.NameToLayer("Agents");
+        layerMask = 1 << layerID;
+    }
+
+    public override Vector3 Force()
+    {
+        Vector3 velocity = agentMovement.velocity;
+        Vector3 normalizedVelocity = velocity.normalized;
+        Vector3 ahead = transform.position + normalizedVelocity * max_queueAhead;
+        colliders = Physics.OverlapSphere(ahead, max_queueRadius, layerMask);
+        if(colliders.Length > 0)
+        {
+            foreach(Collider c in colliders)
+            {
+                if(c.gameObject != this.gameObject && c.gameObject.GetComponent<Movement>().velocity.magnitude < velocity.magnitude)
+                {
+                    agentMovement.velocity *= 0.8f;
+                    break;
+                }
+            }
+        }
+
+        return Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/Steerings/SteeringQueueAvoidObstacle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringQueueAvoidObstacle : SteeringBase
+{
+    public float avoidanceForce;
+    public float max_sightForward;
+
+    private Vector3 desiredVelocity;
+    private Movement agentMovement;
+    private float maxSpeed;
+    private float maxForce;
+    private GameObject[] obstacles;
+    private int layerID;
+    private LayerMask layerMask;
+
+    void Start()
+    {
+        agentMovement = GetComponent<Movement>();
+        maxSpeed = agentMovement.maxSpeed;
+        maxForce = agentMovement.maxForce;
+        if (avoidanceForce > maxForce)
+            avoidanceForce = maxForce;
+
+        obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        layerID = LayerMask.NameToLayer("Obstacle");
+        layerMask = 1 << layerID;
+    }
+
+    public override Vector3 Force()
+    {
+        RaycastHit hit;
+        Vector3 force = Vector3.zero;
+        Vector3 velocity = agentMovement.velocity;
+        Vector3 normalizedVelocity = velocity.normalized;
+        if(Physics.Raycast(transform.position, normalizedVelocity, out hit, max_sightForward, layerMask))
+        {
+            Vector3 ahead = transform.position + normalizedVelocity * max_sightForward;
+            force = ahead - hit.collider.transform.position;
+            force *= avoidanceForce;
+            force.y = 0;
+        }
+        return force;
+    }
+}
